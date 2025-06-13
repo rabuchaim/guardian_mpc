842445ade0ce96793e2598561a5f9184074c6dff
Ricardo Abuchaim <ricardoabuchaim@gmail.com>
2025-06-13 01:23:19 -0300

    Added Guardian MPC

--- mpc_core/settings.py
+++ mpc_core/settings.py
@@ -46,7 +46,7 @@ WSGI_APPLICATION = "mpc_core.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": "mpc_database/db.sqlite3",
+        "NAME": BASE_DIR/"mpc_database/db.sqlite3",
     }
 }
 
